1cdf70511d6567253b5e030fd9496dc25bd43940
jarquafelmu <thechaoselement@gmail.com>
2020-10-15 12:51:40 -0600

    much cleaner using thenables

--- Ch01/01_05/finished/index.js
+++ Ch01/01_05/finished/index.js
@@ -9,33 +9,50 @@ var delay = (seconds) => new Promise((resolves) => {
     setTimeout(resolves, seconds * 1000)
 })
 
-const doStuffSequentially = () => {
-    console.log('starting');
-    setTimeout(() => {
-        console.log('waiting');
-        setTimeout(() => {
-            console.log('waiting some more');
-            fs.writeFile('file.txt', 'Sample File...', error => {
-                if (error) {
-                    console.error(error);
-                } else {
-                    beep();
-                    console.log('file.txt created')
-                    setTimeout(() => {
-                        beep();
-                        fs.unlink('file.txt', error => {
-                            if (error) {
-                                console.error(error);
-                            } else {
-                                console.log('file.txt removed');
-                                console.log('sequential execution complete');
-                            }
-                        })
-                    }, 3000)
-                }
-            });
-        }, 2000)
-    }, 1000)
-}
+const doStuffSequentially = () => Promise.resolve()
+    .then(() => console.log(`starting`))
+    .then(() => delay(1)) // thenable
+    .then(() => `waiting`)
+    .then(console.log)
+    .then(() => delay(2))
+    .then(() => writeFile(`file.txt`, `Sample File...`))
+    .then(beep)
+    .then(() => `file.txt created`)
+    .then(console.log)
+    .then(() => delay(3))
+    .then(() => unlink(`file.txt`))
+    .then(beep)
+    .then(() => `file.txt removed`)
+    .then(console.log)
+    .catch((err) => console.error(err))
+
+// const doStuffSequentially = () => {
+//     console.log('starting');
+//     setTimeout(() => {
+//         console.log('waiting');
+//         setTimeout(() => {
+//             console.log('waiting some more');
+//             fs.writeFile('file.txt', 'Sample File...', error => {
+//                 if (error) {
+//                     console.error(error);
+//                 } else {
+//                     beep();
+//                     console.log('file.txt created')
+//                     setTimeout(() => {
+//                         beep();
+//                         fs.unlink('file.txt', error => {
+//                             if (error) {
+//                                 console.error(error);
+//                             } else {
+//                                 console.log('file.txt removed');
+//                                 console.log('sequential execution complete');
+//                             }
+//                         })
+//                     }, 3000)
+//                 }
+//             });
+//         }, 2000)
+//     }, 1000)
+// }
 
 doStuffSequentially();
