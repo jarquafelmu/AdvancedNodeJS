fcd33599987de79b91ebadf9384b6cca33ca5745
jarquafelmu <thechaoselement@gmail.com>
2020-10-15 12:24:42 -0600

    promise and resolve

--- Ch01/01_02/finished/index.js
+++ Ch01/01_02/finished/index.js
@@ -1,9 +1,14 @@
+var delay = (seconds) => new Promise((resolves, rejects) => {
+    // wait for async process to complete and then resolve it
+    setTimeout(resolves, seconds * 1000);
+});
+
 function delay(seconds, callback) {
-    setTimeout(callback, seconds*1000);
+    setTimeout(callback, seconds * 1000);
 }
 
-delay(1, () => {
-    console.log('one second');
+delay(1).then(() => {
+    console.log('the delay has finished');
 })
 
 console.log('end first tick');
