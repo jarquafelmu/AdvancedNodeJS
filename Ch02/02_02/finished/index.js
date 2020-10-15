f9d44dd6568cbee81936aa44f965ad071e7b9f80
jarquafelmu <thechaoselement@gmail.com>
2020-10-15 17:47:16 -0600

    pass objects to streams

--- Ch02/02_02/finished/index.js
+++ Ch02/02_02/finished/index.js
@@ -13,14 +13,17 @@ const peaks = [
 class StreamFromArray extends Readable {
 
     constructor(array) {
-        super({ encoding: `utf8` }); // invokes the Readable constructor
+        super({ objectMode: true }); // invokes the Readable constructor
         this.array = array;
         this.index = 0;
     }
 
     _read() {
         if (this.index <= this.array.length) {
-            const chunk = this.array[this.index]
+            const chunk = {
+                data: this.array[this.index],
+                index: this.index
+            }
             this.push(chunk)
             this.index += 1;
         } else {
