2a174e33a39d05e4ffe81f04745662a8dc09deee
jarquafelmu <thechaoselement@gmail.com>
2020-10-15 12:25:41 -0600

    passing from promise

--- Ch01/01_02/finished/index.js
+++ Ch01/01_02/finished/index.js
@@ -1,14 +1,16 @@
 var delay = (seconds) => new Promise((resolves, rejects) => {
     // wait for async process to complete and then resolve it
-    setTimeout(resolves, seconds * 1000);
+    setTimeout(() => {
+        resolves(`the long delay has ended`)
+    }, seconds * 1000);
 });
 
 function delay(seconds, callback) {
     setTimeout(callback, seconds * 1000);
 }
 
-delay(1).then(() => {
-    console.log('the delay has finished');
+delay(1).then((message) => {
+    console.log(message);
 })
 
 console.log('end first tick');
