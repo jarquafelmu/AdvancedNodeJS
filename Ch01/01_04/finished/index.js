28e612ec83c248b8c74859a4c711f807e9409b8e
jarquafelmu <thechaoselement@gmail.com>
2020-10-15 12:40:35 -0600

    turn callbacks (even file system) into promises

--- Ch01/01_04/finished/index.js
+++ Ch01/01_04/finished/index.js
@@ -1,24 +1,8 @@
+var fs = require(`fs`)
 var { promisify } = require(`util`)
 
-var delay = (seconds, callback) => {
-    if (seconds > 3) {
-        callback(new Error(`${seconds} seconds it too long!`));
-    } else {
-        setTimeout(() =>
-            callback(null, `the ${seconds} second delay is over.`),
-            seconds
-        );
-    }
-}
+var writeFile = promisify(fs.writeFile);
 
-delay(4, (error, message) => {
-    if (error) {
-        console.log(error.message);
-    } else {
-        console.log(message);
-    }
-});
-
-var promiseDelay = promisify(delay);
-
-promiseDelay(5).then(console.log).catch((error) => console.log(`error: ${error.message}`))
+writeFile(`sample.txt`, `This is a sample`)
+    .then(() => console.log(`file succesfully created`))
+    .catch((err) => { console.log(`error: ${err.message}`) });
