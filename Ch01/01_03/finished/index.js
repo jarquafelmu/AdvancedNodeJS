acfb07deec1e650965742e9f5aa9b78039d01e35
jarquafelmu <thechaoselement@gmail.com>
2020-10-15 12:30:13 -0600

    handling error from promise

--- Ch01/01_03/finished/index.js
+++ Ch01/01_03/finished/index.js
@@ -1,12 +1,14 @@
 var delay = (seconds) => new Promise((resolves, rejects) => {
-    setTimeout(() => {
-        resolves('the long delay has ended')
-    }, time);
+  throw new Error(`argh`)
+  setTimeout(() => {
+    resolves('the long delay has ended')
+  }, time);
 });
 
 delay(1)
   .then(console.log)
   .then(() => 42)
-  .then((number) => console.log('Hello world: ${number}'));
+  .then((number) => console.log('Hello world: ${number}'))
+  .catch((err) => console.log(`error: ${err.message}`));
 
 console.log('end first tick');
