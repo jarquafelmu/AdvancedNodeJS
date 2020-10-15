71ad67d0e94cef225cb16af6ac4e46a71f9014ec
jarquafelmu <thechaoselement@gmail.com>
2020-10-15 16:16:23 -0600

    using await and async

--- Ch01/01_05/finished/index.js
+++ Ch01/01_05/finished/index.js
@@ -9,50 +9,23 @@ var delay = (seconds) => new Promise((resolves) => {
     setTimeout(resolves, seconds * 1000)
 })
 
-const doStuffSequentially = () => Promise.resolve()
-    .then(() => console.log(`starting`))
-    .then(() => delay(1)) // thenable
-    .then(() => `waiting`)
-    .then(console.log)
-    .then(() => delay(2))
-    .then(() => writeFile(`file.txt`, `Sample File...`))
-    .then(beep)
-    .then(() => `file.txt created`)
-    .then(console.log)
-    .then(() => delay(3))
-    .then(() => unlink(`file.txt`))
-    .then(beep)
-    .then(() => `file.txt removed`)
-    .then(console.log)
-    .catch((err) => console.error(err))
+const doStuffSequentially = async () => {
+    console.log(`starting`)
+    await delay(1);
+    console.log(`waiting`);
+    await delay(2)
+    try {
+        await writeFile(`file.txt`, `Sample File...`)
+        beep()
+    } catch (error) {
+        console.error(error)
+    }
+    console.log(`file.txt created`)
+    await delay(3)
+    await unlink(`file.txt`)
+    console.log(`file.txt removed`)
 
-// const doStuffSequentially = () => {
-//     console.log('starting');
-//     setTimeout(() => {
-//         console.log('waiting');
-//         setTimeout(() => {
-//             console.log('waiting some more');
-//             fs.writeFile('file.txt', 'Sample File...', error => {
-//                 if (error) {
-//                     console.error(error);
-//                 } else {
-//                     beep();
-//                     console.log('file.txt created')
-//                     setTimeout(() => {
-//                         beep();
-//                         fs.unlink('file.txt', error => {
-//                             if (error) {
-//                                 console.error(error);
-//                             } else {
-//                                 console.log('file.txt removed');
-//                                 console.log('sequential execution complete');
-//                             }
-//                         })
-//                     }, 3000)
-//                 }
-//             });
-//         }, 2000)
-//     }, 1000)
-// }
+    return Promise.resolve();
+}
 
 doStuffSequentially();
