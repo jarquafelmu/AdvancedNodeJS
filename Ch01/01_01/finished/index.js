36f1e43d40da41068ddea553a08466af3f4d39fe
jarquafelmu <thechaoselement@gmail.com>
2020-10-15 12:17:05 -0600

    callback

--- Ch01/01_01/finished/index.js
+++ Ch01/01_01/finished/index.js
@@ -1,5 +1,7 @@
 function hideString(str, done) {
   process.nextTick(() => {
+    // this is a callback since it doesn't fire in order of the
+    // code on the page, it fires when it is ready
     done(str.replace(/[a-zA-Z]/g, 'X'));
   })
 }
