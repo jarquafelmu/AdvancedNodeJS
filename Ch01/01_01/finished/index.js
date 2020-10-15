20e8cd033eb76f230df4b0751d797bbc3bfeb6a0
jarquafelmu <thechaoselement@gmail.com>
2020-10-15 12:18:20 -0600

    creating asynchronous code

--- Ch01/01_01/finished/index.js
+++ Ch01/01_01/finished/index.js
@@ -1,13 +1,8 @@
-function hideString(str, done) {
-  process.nextTick(() => {
-    // this is a callback since it doesn't fire in order of the
-    // code on the page, it fires when it is ready
-    done(str.replace(/[a-zA-Z]/g, 'X'));
-  })
+function delay(seconds, callback) {
+  setTimeout(callback, seconds * 1000);
 }
 
-hideString("Hello World", (hidden) => {
-  console.log(hidden);
-});
-
-console.log("end")
+console.log(`starting delays`)
+delay(2, () => {
+  console.log(`two seconds`)
+})
