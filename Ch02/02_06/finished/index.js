8b6a006d78c78f940f57a433d85401e3923a5a2a
jarquafelmu <thechaoselement@gmail.com>
2020-10-15 19:50:09 -0600

    using unix pipe

--- Ch02/02_06/finished/index.js
+++ Ch02/02_06/finished/index.js
@@ -3,3 +3,6 @@ const { createReadStream, createWriteStream } = require('fs');
 const writeStream = createWriteStream('./file.txt');
 
 process.stdin.pipe(writeStream)
+
+// console:
+// echo "Hello World" | node .
