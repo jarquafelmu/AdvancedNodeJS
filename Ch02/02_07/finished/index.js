c17860458cc55df984fd1467ef6d991e101c35af
jarquafelmu <thechaoselement@gmail.com>
2020-10-15 19:56:45 -0600

    duplex stream

---
+++ Ch02/02_07/finished/index.js
@@ -0,0 +1,17 @@
+const { PassThrough } = require(`stream`)
+const { createReadStream, createWriteStream } = require('fs');
+
+const readStream = createReadStream('../../powder-day.mp4')
+const writeStream = createWriteStream('./copy.mp4');
+
+const report = new PassThrough();
+
+let total = 0;
+report.on(`data`, (chunk) => {
+  total += chunk.length;
+  console.log(`bytes: ${total}`)
+})
+
+readStream
+  .pipe(report)
+  .pipe(writeStream);
